3d338d14e2985ee3e6a8299170b302aa3dc13524
raffael-carvalho <ffaelcdec@gmail.com>
2025-10-26 22:44:29 -0300

    Correção do teste

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./app"); // Importamos nosso app
 
 describe("API Olá Mundo", () => {
-  it('Deve retornar "Olá Mundo DevOps!" na rota /', async () => {
+  it('Deve retornar "Olá Mundo DevOps, Proteger a Branch main!" na rota /', async () => {
     // Faz uma requisição GET para a rota /
     const response = await request(app).get("/");
 
@@ -10,6 +10,6 @@ describe("API Olá Mundo", () => {
     expect(response.statusCode).toBe(200);
 
     // Verifica se o corpo da resposta é o esperado
-    expect(response.text).toBe("Olá Mundo DevOps!");
+    expect(response.text).toBe("Olá Mundo DevOps, Proteger a Branch main!");
   });
 });
